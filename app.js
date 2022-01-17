0454b3760402185cc32fe7a1a3e03b80d2cd7344
eunice-hong <dev@eunice-hong.kr>
2022-01-17 15:37:09 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const request = require("request");
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/search", function (req, res) {
+  const options = {
+    uri: process.env.HOST,
+    qs: {
+      query: req.query.query,
+      display: 20,
+    },
+    headers: {
+      "X-Naver-Client-Id": process.env.NAVER_CLIENT_ID,
+      "X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRET,
+    },
+  };
+
+  request.get(options, (err, httpResponse, body) => {
+    if (!err && httpResponse.statusCode == 200) {
+      console.log(httpResponse.body);
+      res.send(httpResponse.body);
+    } else {
+      console.log(err);
+      console.log(httpResponse.statusCode);
+      res.send(err);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
