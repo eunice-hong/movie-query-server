3054cab99f35b947f5083e0149bc53534ee975ec
eunice-hong <dev@eunice-hong.kr>
2022-01-17 17:12:07 +0900

    feat: add cors

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
