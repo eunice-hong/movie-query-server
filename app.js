87ca94209216fe9e8a730bcc041525772e498ed7
eunice-hong <dev@eunice-hong.kr>
2022-01-27 23:15:56 +0900

    feat: set new search api

--- app.js
+++ app.js
@@ -6,37 +6,92 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 
+var REQUEST_TOKEN = "";
+var EXPIRES_AT = "";
+var SESSION_ID = "";
+
+const BASE_HEADER = {
+  Authorization: "Bearer " + process.env.THE_MOVIE_DB_API_TOKEN,
+  "Content-Type": "application/json;charset=utf-8",
+};
+
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get("/", (req, res) => {});
 
 app.get("/search", function (req, res) {
   const options = {
-    uri: process.env.HOST,
+    headers: BASE_HEADER,
+    uri: process.env.HOST + "/search/movie",
     qs: {
       query: req.query.query,
-      display: 20,
-    },
-    headers: {
-      "X-Naver-Client-Id": process.env.NAVER_CLIENT_ID,
-      "X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRET,
+      api_key: process.env.THE_MOVIE_DB_API_KEY,
+      language: "ko-KR",
+      page: req.query.page,
+      include_adult: false,
     },
   };
 
   request.get(options, (err, httpResponse, body) => {
     if (!err && httpResponse.statusCode == 200) {
-      console.log(httpResponse.body);
       res.send(httpResponse.body);
     } else {
-      console.log(err);
       console.log(httpResponse.statusCode);
       res.send(err);
     }
   });
 });
 
+function main() {
+  getRequestToken();
+}
+
+function getRequestToken() {
+  const options = {
+    headers: BASE_HEADER,
+    uri: process.env.HOST + "/authentication/token/new",
+    qs: {
+      api_key: process.env.THE_MOVIE_DB_API_KEY,
+    },
+  };
+
+  request.get(options, (err, httpResponse, body) => {
+    if (!err && httpResponse.statusCode == 200) {
+      REQUEST_TOKEN = httpResponse.body.request_token;
+      EXPIRES_AT = httpResponse.body.expires_at;
+      getSessionId();
+    } else {
+      console.log(err);
+      console.log(httpResponse.statusCode);
+      res.send(err);
+    }
+  });
+}
+
+function getSessionId() {
+  const options = {
+    headers: BASE_HEADER,
+    uri: process.env.HOST + "/authentication/guest_session/new",
+    qs: {
+      api_key: process.env.THE_MOVIE_DB_API_KEY,
+    },
+  };
+  const body = {
+    request_token: REQUEST_TOKEN,
+  };
+
+  request.post(options, (err, httpResponse, body) => {
+    if (!err && httpResponse.statusCode == 200) {
+      SESSION_ID = httpResponse.body.guest_session;
+    } else {
+      console.log(err);
+      console.log(httpResponse.statusCode);
+      res.send(err);
+    }
+  });
+}
+
 app.listen(port, () => {
+  main();
   console.log(`Example app listening at ${port}`);
 });
